d6097338c4d248c4e67f7bf1422bceaf102efe0c
Micaelmf <micaelmf@gmail.com>
2016-11-23 20:43:25 -0300

    alterações da aula do dia 23/11

---
+++ ProjetoWEB1/src/Include.java
@@ -0,0 +1,48 @@
+
+import Model.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Include {
+    
+    public static void main(String[] args) throws SQLException{
+        //conectando
+        Connection con = new ConnectionFactory().getConnection();
+        if(con != null){
+            System.out.println("Conexão: DEU CERTO =)");
+        }else{
+            System.out.println("Conexão: PRESTOU NÃO!!!");
+        }
+        /*
+        //cria um praparedStatement
+        String sql = "insert int membros" +
+                " (id, nome_completo, cpf, rg, orgao_exp, curso, email, celular, "
+                + "logradouro, numero, referencia, bairro, cidade, estado, aceito)" +
+                " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        //preenche os valores
+        stmt.setString(1, "texto");
+        stmt.setString(2, "texto");
+        stmt.setString(3, "123");
+        stmt.setString(4, "456");
+        stmt.setString(5, "texto");
+        stmt.setString(6, "texto");
+        stmt.setString(7, "texto");
+        stmt.setString(8, "texto");
+        stmt.setString(9, "texto");
+        stmt.setString(10, "789");
+        stmt.setString(11, "texto");
+        stmt.setString(12, "texto");
+        stmt.setString(13, "texto");
+        stmt.setString(14, "texto");
+        stmt.setString(15, "texto");
+        
+        stmt.execute();
+        stmt.close();
+        System.out.println("Gravado!!!");
+        con.close();
+        */
+    }
+    
+}
--- ProjetoWEB1/src/Model/ConnectionFactory.java
+++ ProjetoWEB1/src/Model/ConnectionFactory.java
@@ -11,40 +11,7 @@ public class ConnectionFactory {
             return DriverManager.getConnection("jdbc:mysql://localhost/include","root","");
         }catch(SQLException e){
             throw new RuntimeException(e);
+            //logger.getlogger(ConnectionFactory.class.getName()).log(level.SEVERE null ex);
         }
-    }    
-    
-    public static void main(String[] args) throws SQLException{
-        //conectando
-        Connection con = new ConnectionFactory().getConnection();
-        //cria um praparedStatement
-        String sql = "insert int membros" +
-                " (id, nome_completo, cpf, rg, orgao_exp, curso, email, celular, "
-                + "logradouro, numero, referencia, bairro, cidade, estado, aceito)" +
-                " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        PreparedStatement stmt = con.prepareStatement(sql);
-        //preenche os valores
-        stmt.setString(1, "texto");
-        stmt.setString(2, "texto");
-        stmt.setString(3, "123");
-        stmt.setString(4, "456");
-        stmt.setString(5, "texto");
-        stmt.setString(6, "texto");
-        stmt.setString(7, "texto");
-        stmt.setString(8, "texto");
-        stmt.setString(9, "texto");
-        stmt.setString(10, "789");
-        stmt.setString(11, "texto");
-        stmt.setString(12, "texto");
-        stmt.setString(13, "texto");
-        stmt.setString(14, "texto");
-        stmt.setString(15, "texto");
-        
-        stmt.execute();
-        stmt.close();
-        System.out.println("Gravado!!!");
-        con.close();
-        
-    }
-            
+    }            
 }
