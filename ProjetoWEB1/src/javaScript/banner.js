4ba91908c16dd6d32bbb17398fd2a8a17af0dacb
Micaelmf <micaelmf@gmail.com>
2016-12-05 12:22:19 -0300

    Alteração

--- ProjetoWEB1/src/javaScript/banner.js
+++ ProjetoWEB1/src/javaScript/banner.js
@@ -67,21 +67,21 @@ function buildImage() {
 }
 
 function proxImagem(){
-  var img = document.getElementById('content').getElementsByTagName('img')[0]
-  indice++;
-  indice = indice % imagens.length 
-  img.src = imagens[indice];
+    var img = document.getElementById('content').getElementsByTagName('img')[0]
+    indice++;
+    indice = indice % imagens.length 
+    img.src = imagens[indice];
 }
 
 function imagemAnt(){
-	var img = document.getElementById('content').getElementsByTagName('img')[0]
-	if(indice == 0){
-		indice = imagens.length-1;
-		img.src = imagens[indice];	
-	}else{
-		indice--;
-		img.src = imagens[indice];
-	}
+    var img = document.getElementById('content').getElementsByTagName('img')[0]
+    if(indice == 0){
+        indice = imagens.length-1;
+        img.src = imagens[indice];	
+    }else{
+        indice--;
+        img.src = imagens[indice];
+    }
 }
 
 setInterval(proxImagem, 5000);
